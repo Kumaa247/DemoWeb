7df9d27d2ec9a871aefca4afabcacfee93b64efa
Kumaa247 <101690524+Kumaa247@users.noreply.github.com>
2022-10-22 12:00:02 +0700

    aksjdnk

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -9,7 +9,7 @@ using DemoWeb.Models;
 namespace DemoWeb.Controllers
 {
     // jđaookaênoiạdaksnalkdnàlksdfnăd
-    public class khongcogi { }
+    public class Angang { }
     public class CategoriesController : Controller
     {
         DBSportStoreEntities database = new DBSportStoreEntities();
